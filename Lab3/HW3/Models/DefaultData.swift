495bceeda45553ce1a560d6b562e3a13d8312d6c
Pavel Bakharuev <pavel5609s@gmail.com>
2023-10-09 17:19:27 +0300

    Lab3 , done, but works at horizontall position.

---
+++ Lab3/HW3/Models/DefaultData.swift
@@ -0,0 +1,11 @@
+//
+//  DefaultData.swift
+//  HW3
+//
+//  Created by Anna Dluzhinskaya on 02.10.2023.
+//
+
+struct DefaultData {
+    var name: String = "Zhaba"
+    var education: String = "IU"
+}
---
+++ Practice3/Practice3/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Practice3
+//
+//  Created by pbakharuev on 02.10.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
