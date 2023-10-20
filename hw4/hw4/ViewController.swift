c2e2beae32e6d98adb18730ef747198c453c2a32
Pavel Bakharuev <pavel5609s@gmail.com>
2023-10-20 22:45:16 +0300

    fixes

---
+++ hw4/hw4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw4
+//
+//  Created by pbakharuev on 20.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
