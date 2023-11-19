b0631290f9e7720b322d92c5477aaf25ef0d1f06
Pavel Bakharuev <pavel5609s@gmail.com>
2023-11-19 21:44:09 +0300

    Likes

--- Book of the day/Book of the day/Books.swift
+++ Book of the day/Book of the day/Books.swift
@@ -12,25 +12,57 @@ struct Book {
     let author: String
     let image: String
     let amazonURL: String
+    let eUrl : String
+    let isLiked: Bool
 }
 
 // Mock-up data
-let bookList: [Book] = [
+var bookList: [Book] = [
     Book(
         title: "SWEET TOMORROWS",
         description: "The journeys of the characters at the Rose Harbor Inn come to a close in this last book of the series.",
         author: "Debbie Macomber",
         image: "https://storage.googleapis.com/du-prd/books/images/9780553391848.jpg",
-        amazonURL: "https://www.ozon.ru/product/sweet-tomorrows-1265193626/"
+        amazonURL: "https://www.ozon.ru/product/sweet-tomorrows-1265193626/",
+        eUrl: "https://books.google.ru/books?id=MijjCgAAQBAJ&hl=ru",
+        isLiked: false
     ),
     Book(
         title: "THE MURDER HOUSE",
         description: "When bodies are found at a Hamptons estate where a series of grisly murders once occurred, a local detective and former New York City cop investigates.",
-        author: "James Patterson and David Ellis",
+        author: "J.Patterson, D. Ellis",
         image: "https://storage.googleapis.com/du-prd/books/images/9780316337977.jpg",
-        amazonURL: "http://www.amazon.com/The-Murder-House-James-Patterson-ebook/dp/B00QQQL8JY?tag=NYTBSREV-20"
+        amazonURL: "http://www.amazon.com/The-Murder-House-James-Patterson-ebook/dp/B00QQQL8JY?tag=NYTBSREV-20",
+        eUrl: "https://books.google.ru/books?id=MijjCgAAQBAJ&hl=ru",
+        isLiked: false
     ),
-    // Add more books as needed
+    Book(
+        title: "A DOG'S PURPOSE",
+        description: "From stray mutt to golden-haired puppy, a dog finds himself reincarnated over the years as he searches for his purpose in life.",
+        author: "W Bruce Cameron",
+        image: "https://storage.googleapis.com/du-prd/books/images/9780765330345.jpg",
+        amazonURL: "https://www.amazon.com/Dogs-Purpose-Novel-Humans/dp/0765330342?tag=NYTBSREV-20",
+        eUrl: "https://books.google.ru/books?id=MijjCgAAQBAJ&hl=ru",
+        isLiked: false
+    ),
+    Book(
+        title: "MULBERRY MOON",
+        description: "Ben Sterling returns home to Mystic Creek to settle down and start a family, but the woman of his interest is more than a little resistant. As she warms to him, her past comes back to interfere.",
+        author: "Catherine Anderson",
+        image: "https://storage.googleapis.com/du-prd/books/images/9780451488022.jpg",
+        amazonURL: "https://www.amazon.com/Mulberry-Mystic-Creek-Catherine-Anderson/dp/0451488024?tag=NYTBSREV-20",
+        eUrl: "https://books.google.ru/books?id=MijjCgAAQBAJ&hl=ru",
+        isLiked: false
+    ),
+    Book(
+        title: "CRASH AND BURN",
+        description: "The Sisterhood steps up when the legal client of a member is threatened by ruthless politics and ambition in Washington.",
+        author: "Fern Michaels",
+        image: "https://storage.googleapis.com/du-prd/books/images/9781420140651.jpg",
+        amazonURL: "https://www.amazon.com/Crash-Burn-Sisterhood-Fern-Michaels/dp/1420140655?tag=NYTBSREV-20",
+        eUrl: "https://books.google.ru/books?id=MijjCgAAQBAJ&hl=ru",
+        isLiked: false
+    )
 ]
 
 // Now you can use this mock data for testing or displaying in your app.
--- Book of the day/Book of the day/ContentView.swift
+++ Book of the day/Book of the day/ContentView.swift
@@ -56,16 +56,49 @@ struct BuyLink: Codable {
 
 class ViewModel: ObservableObject {
     @Published var book: Book = bookList.first!
-    var i = 1
+    @Published var nextBook: Book = bookList[1]
+    var i = 0
     let url = URL(string: "https://api.nytimes.com/svc/books/v3/lists/current/mass-market-paperback?api-key=ZtG15qqgQeLGtIn7K6hVb57qY4ombxhx")!
-    
+    var showingAlert = false
+    var likes = [-1, -2]
+    var isLiked = false
     func next(){
-        DispatchQueue.main.async {
-            if (self.i < 3){
+        if (self.i + 1 < bookList.endIndex){
+            self.i = self.i + 1
+            DispatchQueue.main.async {
                 self.book = bookList[self.i]
-                self.i = self.i + 1
+                if (self.likes.contains(self.i)){
+                    self.isLiked = true
+                } else {
+                    self.isLiked = false
+                }
             }
+        } else {
+            self.showingAlert = true
         }
+        
+    }
+    func restart(){
+        DispatchQueue.main.async {
+            self.i = 0
+            self.book = bookList[self.i]
+            self.showingAlert = false
+        }
+        
+    }
+    func like(){
+        if (isLiked) {
+            unlike()
+        } else {
+            self.isLiked.toggle()
+            self.likes.append(self.i)
+        }
+        
+    }
+    
+    func unlike(){
+        self.isLiked.toggle()
+        self.likes.removeAll(where: { $0 == i } )
     }
     func fetch(){
         var request = URLRequest(url: url,timeoutInterval: Double.infinity)
@@ -81,7 +114,6 @@ class ViewModel: ObservableObject {
 //          print(String(data: data, encoding: .utf8)!)
             do {
                 let bookResponse = try JSONDecoder().decode(BookResponse.self, from: data)
-                // Now you can access the data like bookResponse.results.listName, bookResponse.results.books, etc.
                 print(bookResponse.results)
                 guard let firstBook = bookResponse.results.books.first else {
                             print("No books found in the response")
@@ -102,24 +134,66 @@ class ViewModel: ObservableObject {
 
 struct ContentView: View {
     @StateObject var viewModel = ViewModel()
+    @State var showingAlert : Bool = false
+    @State var isLiked: Bool = false
     var body: some View {
         VStack{
             Text("Book of the day by NYT")
                 .font(.system(size: 32, weight: .medium, design: .default))
                 .foregroundColor(.black)
                 .padding()
-            Image("sweet_tommorovs")
+            Image("img_" + String(viewModel.i))
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 350, height: 450)
-            Link("Buy now", destination: URL(string: viewModel.book.amazonURL)!)
-            Text(viewModel.book.title + " by " + viewModel.book.author).font(.system(size: 20))
-                .frame(width: 300)
+                .frame(width: 350, height: 400)
+            HStack{
+                Link("Buy paper", destination: URL(string: viewModel.book.amazonURL)!)
+                Text("|")
+                Link("Buy e-book", destination: URL(string: viewModel.book.amazonURL)!)
+                Text("|")
+                ShareLink(item: URL(string: viewModel.book.eUrl)!)
+            }
+            HStack{
+                VStack{
+                    Text(viewModel.book.title ).font(.system(size: 20))
+                        .frame(width: 300)
+                        .padding(1)
+                        .lineLimit(2)
+                    Text(" by " + viewModel.book.author).font(.system(size: 20))
+                        .padding(1)
+                        .lineLimit(1)
+                }
+                .frame(width: 250)
+                Button (action:{
+                    viewModel.likes.append(viewModel.i)
+                    print(viewModel.likes)
+                    print(viewModel.i)
+                    viewModel.like()
+                    print(viewModel.isLiked)
+                    isLiked.toggle()
+                }, label: {
+                    Image(systemName: isLiked ? "heart.fill" : "heart")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 25)
+                        .foregroundColor(isLiked ? .red : .gray)
+                })
+                
+                
+            }
+            
+            Text(viewModel.book.description).font(.system(size: 15, design: .default)).frame(width: 350, height: 90)
                 .padding(1)
-            Text(viewModel.book.description).font(.system(size: 15, design: .default)).frame(width: 350, height: 50)
-                .padding()
             Button{
                 viewModel.next()
+                showingAlert = viewModel.showingAlert
+                if (viewModel.likes.contains((viewModel.i + 1) % (viewModel.likes.endIndex + 1))){
+                    isLiked = true
+                    print("след лайк")
+                } else {
+                    isLiked = false
+                }
+                
                 print("btn clicked")
             } label: {
                 Text("Next")
@@ -130,6 +204,9 @@ struct ContentView: View {
                     .cornerRadius(10)
             }
             Spacer()
+        }
+        .alert(isPresented: $showingAlert){
+            Alert(title: Text("Last book"), message: Text("This is the last book for today"), primaryButton: .destructive(Text("Go to first")){ viewModel.restart()}, secondaryButton: .cancel())
         }
 //        .task {
 //            do{
