81e913783617a5521193fd5243b79ca21352da8e
Pavel Bakharuev <pavel5609s@gmail.com>
2023-11-19 15:05:58 +0300

    fp

--- Book of the day/Book of the day/Books.swift
+++ Book of the day/Book of the day/Books.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+struct Book {
+    let title: String
+    let description: String
+    let author: String
+    let image: String
+    let amazonURL: String
+}
+
+// Mock-up data
+let bookList: [Book] = [
+    Book(
+        title: "SWEET TOMORROWS",
+        description: "The journeys of the characters at the Rose Harbor Inn come to a close in this last book of the series.",
+        author: "Debbie Macomber",
+        image: "https://storage.googleapis.com/du-prd/books/images/9780553391848.jpg",
+        amazonURL: "https://www.ozon.ru/product/sweet-tomorrows-1265193626/"
+    ),
+    Book(
+        title: "THE MURDER HOUSE",
+        description: "When bodies are found at a Hamptons estate where a series of grisly murders once occurred, a local detective and former New York City cop investigates.",
+        author: "James Patterson and David Ellis",
+        image: "https://storage.googleapis.com/du-prd/books/images/9780316337977.jpg",
+        amazonURL: "http://www.amazon.com/The-Murder-House-James-Patterson-ebook/dp/B00QQQL8JY?tag=NYTBSREV-20"
+    ),
+    // Add more books as needed
+]
+
+// Now you can use this mock data for testing or displaying in your app.
--- Book of the day/Book of the day/ContentView.swift
+++ Book of the day/Book of the day/ContentView.swift
@@ -7,18 +7,139 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+import Foundation
+
+struct BookResponse: Codable {
+    let status: String
+    let copyright: String
+    let numResults: Int
+    let lastModified: String
+    let results: BookResult
 }
 
+struct BookResult: Codable {
+    let listName: String
+    let listNameEncoded: String
+    let bestsellersDate: String
+    let publishedDate: String
+    let publishedDateDescription: String
+    let nextPublishedDate: String?
+    let previousPublishedDate: String
+    let displayName: String
+    let normalListEndsAt: Int
+    let updated: String
+    let books: [BookDetails]
+}
+
+struct BookDetails: Codable {
+    let description: String
+    let price: String
+    let title: String
+    let author: String
+    let bookImage: String
+    let amazonProductURL: String
+    let isbns: [ISBN]
+    let buyLinks: [BuyLink]
+    let bookURI: String
+}
+
+struct ISBN: Codable {
+    let isbn10: String
+    let isbn13: String
+}
+
+struct BuyLink: Codable {
+    let name: String
+    let url: String
+}
+
+
+class ViewModel: ObservableObject {
+    @Published var book: Book = bookList.first!
+    var i = 1
+    let url = URL(string: "https://api.nytimes.com/svc/books/v3/lists/current/mass-market-paperback?api-key=ZtG15qqgQeLGtIn7K6hVb57qY4ombxhx")!
+    
+    func next(){
+        DispatchQueue.main.async {
+            if (self.i < 3){
+                self.book = bookList[self.i]
+                self.i = self.i + 1
+            }
+        }
+    }
+    func fetch(){
+        var request = URLRequest(url: url,timeoutInterval: Double.infinity)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        request.httpMethod = "GET"
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            return
+          }
+//          print(String(data: data, encoding: .utf8)!)
+            do {
+                let bookResponse = try JSONDecoder().decode(BookResponse.self, from: data)
+                // Now you can access the data like bookResponse.results.listName, bookResponse.results.books, etc.
+                print(bookResponse.results)
+                guard let firstBook = bookResponse.results.books.first else {
+                            print("No books found in the response")
+                            return
+                        }
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        }
+        
+
+        task.resume()
+        
+        return
+    }
+
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    var body: some View {
+        VStack{
+            Text("Book of the day by NYT")
+                .font(.system(size: 32, weight: .medium, design: .default))
+                .foregroundColor(.black)
+                .padding()
+            Image("sweet_tommorovs")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 350, height: 450)
+            Link("Buy now", destination: URL(string: viewModel.book.amazonURL)!)
+            Text(viewModel.book.title + " by " + viewModel.book.author).font(.system(size: 20))
+                .frame(width: 300)
+                .padding(1)
+            Text(viewModel.book.description).font(.system(size: 15, design: .default)).frame(width: 350, height: 50)
+                .padding()
+            Button{
+                viewModel.next()
+                print("btn clicked")
+            } label: {
+                Text("Next")
+                    .frame(width: 200, height: 40)
+                    .background(Color.blue)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            Spacer()
+        }
+//        .task {
+//            do{
+//                print("hello")
+//            } catch {
+//
+//            }
+        }
+    }
+   
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
