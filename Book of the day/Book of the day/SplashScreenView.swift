e300f345af60052f00190d1d565243a7b35dbcab
Pavel Bakharuev <pavel5609s@gmail.com>
2023-11-20 19:39:50 +0300

    Add preview

---
+++ Book of the day/Book of the day/SplashScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreenView.swift
+//  Book of the day
+//
+//  Created by pbakharuev on 20.11.2023.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
---
+++ Book of the day/Book of the day/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Book of the day
+//
+//  Created by pbakharuev on 20.11.2023.
+//
+
+import Foundation
